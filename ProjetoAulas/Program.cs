b13b859b7138f7cd9628eb54a81f9176f8630116
Guilherme Chaves <gb.chaves@hotmail.com>
2023-03-29 11:16:00 -0300

    csharp2

---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Primeiro programa");
+
+// int idade = 23;
+// Console.WriteLine(idade);
+// string nomePessoa = "Rafael Almeida";
+// Console.WriteLine(nomePessoa);
+// decimal valor = 200.99m;
+// double valorDouble = 200.99;
+// float valorFloat = 200.99f;
+// Console.WriteLine(valor);
+// Console.WriteLine(valorDouble);
+// Console.WriteLine(valorFloat);
+// var idadeNova = 34;
+// char flag = 'Y';
+// Console.WriteLine(flag);
+// bool ativo = true;
+// Console.WriteLine(ativo);
+
+// const string descricao = "CursoCSHARP";
+// descricao = "Curso";
+
+// Console.WriteLine(descricao);
+
+int numero1 = 1;
+var numero2 = 2;
+
+// int soma = numero1 + numero2;
+
+// Console.WriteLine(soma);
+
+// int subtracao = numero2 - numero1;
+// Console.WriteLine(subtracao);
+
+// int multiplicacao = (numero2 * numero1) * 10;
+// Console.WriteLine(multiplicacao);
+
+// int divisao = numero2 / numero1;
+// Console.WriteLine(divisao);
+
+// bool igual = numero1 == numero2;
+// Console.WriteLine(igual);
+
+// bool menor = numero2 < numero1;
+// Console.WriteLine(menor);
+
+// bool menorOuIgual = numero2 <= numero1;
+// Console.WriteLine(menorOuIgual);
+
+// bool maiorOuIgual = numero2 >= numero1;
+// Console.WriteLine(maiorOuIgual);
+
+// bool diferente = numero2 != numero1;
+// Console.WriteLine(diferente);
+
+
+// bool valido = numero2 > numero1 && 6 > 7;
+// Console.WriteLine(valido);
+
+// bool valido2 = numero2 > 10 || 6 > 7;
+// Console.WriteLine(valido2);
+
+// bool valido3 = !(numero2 > 3);
+// Console.WriteLine(valido3);
+
+// bool ativo = false;
+// string status = ativo ? "Cadastro Ativo" : "Cadastro inativo";
+// Console.WriteLine(status);
+
+// void EscreverNome()
+// {
+//   var nome = NomeCompleto();
+//   var soma = SomaValores();
+
+//   Console.WriteLine(nome);
+//   Console.WriteLine(soma)
+// }
+
+// string NomeCompleto()
+// {
+//   string primeiroNome = "Rafael";
+//   string segundoNome = "Almeida";
+
+//   return primeiroNome + "" + segundoNome;
+// }
+
+// int SomaValores()
+// {
+//   return 8 + 2;
+// }
+
+// var soma = SomaValores(3, 2);
+
+// Console.WriteLine(soma);
+
+// int SomaValores(int a, int b)
+// {
+//   return a + b;
+// }
